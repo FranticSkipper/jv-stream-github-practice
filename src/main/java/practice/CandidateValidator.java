924da12f1f523a56a3898d7e0d1ef92194feb990
Oleg <oleg@gmail.com>
2025-10-15 00:51:03 +0300

    implement StreamPractice class and CandidateValidator predicate class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String PERIOD_SEPARATOR = "-";
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
+    private static final int MIN_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && REQUIRED_NATIONALITY.equals(candidate.getNationality())
+                && this.getPeriodInUkr(candidate.getPeriodsInUkr()) >= MIN_PERIOD;
+    }
+
+    private int getPeriodInUkr(String period) {
+        String[] values = period.split(PERIOD_SEPARATOR);
+        return Integer.parseInt(values[TO_YEAR]) - Integer.parseInt(values[FROM_YEAR]);
+    }
 }
